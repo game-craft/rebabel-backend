030d0c362a5a78d0f3e1e847b8be5abb80554a10
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-05-20 22:44:13 +0900

    Delete 000004_create_chats_dictionary.up.sql

--- migration/000004_create_chats_dictionary.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `chats_dictionary` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `chats_dictionary_content` VARCHAR(255) NOT NULL,
-    `chats_dictionary_status` VARCHAR(255) NOT NULL,
-    `created_at` TIMESTAMP NOT NULL,
-    `updated_at` TIMESTAMP NOT NULL,
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
